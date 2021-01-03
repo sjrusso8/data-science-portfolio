aac934179d5f340bb4a4825183bc6d975629c8d7
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-01-03 09:48:16 -0500

    added in tailwind css

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: {
+        enabled: true,
+        content: ['../django-website/**/templates/*.html'],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
