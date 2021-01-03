869239b28d8cb0aa73f2f9d72b6066a47789f354
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-01-03 10:32:24 -0500

    created tailwind color theme

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            black: "#000",
+            white: "#fff",
+            gray: {
+                100: "f5f5f5",
+                500: "e5e5e5",
+                700: "e0e0e0",
+                900: "b8b8b8"
+            },
+            red: "#df2e01",
+            orange: {
+                100: "#FDB849",
+                300: "#FDA821",
+                500: "#F29602",
+                700: "#CA7D02",
+                900: "#A26402"
+            },
+            metal: {
+                500: "#002E3d",
+                700: "#001F29"
+            }
+        },
         extend: {},
     },
     variants: {
