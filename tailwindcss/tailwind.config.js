6d03affbd7be81e3692e84dea1359efc28039a09
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2021-01-03 20:26:08 -0500

    finished rendering text

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,36 +1,33 @@
 module.exports = {
     purge: {
-        enabled: true,
+        enabled: false,
         content: ['../django-website/**/templates/*.html'],
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
-        colors: {
-            black: "#000",
-            white: "#fff",
-            gray: {
-                100: "f5f5f5",
-                500: "e5e5e5",
-                700: "e0e0e0",
-                900: "b8b8b8"
-            },
-            red: "#df2e01",
-            orange: {
-                100: "#FDB849",
-                300: "#FDA821",
-                500: "#F29602",
-                700: "#CA7D02",
-                900: "#A26402"
-            },
-            metal: {
-                500: "#002E3d",
-                700: "#001F29"
+        extend: {
+            colors: {
+                "off-gray": {
+                    100: "f5f5f5",
+                    500: "e5e5e5",
+                    700: "e0e0e0",
+                    900: "b8b8b8"
+                },
+                "p-red": "#df2e01",
+                "p-orange": {
+                    100: "#FDB849",
+                    300: "#FDA821",
+                    500: "#F29602",
+                    700: "#CA7D02",
+                    900: "#A26402"
+                },
+                "g-metal": {
+                    500: "#002E3d",
+                    700: "#001F29"
+                }
             }
-        },
-        extend: {},
+        }
     },
-    variants: {
-        extend: {},
-    },
+    variants: {},
     plugins: [],
 }
